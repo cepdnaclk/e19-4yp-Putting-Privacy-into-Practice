27d6a6199756ed7d8eabef6dc9e7f5467dbdb55b
MohamedFahman <e19105@eng.pdn.ac.lk>
2025-05-29 22:33:57 +0530

    fixed sidebar

--- code/client/src/components/Layout.jsx
+++ code/client/src/components/Layout.jsx
@@ -2,9 +2,9 @@ import Sidebar from "./Sidebar";
 
 export default function Layout({ children }) {
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen overflow-hidden">
       <Sidebar />
-      <main>{children}</main>
+      <main className="flex-1 overflow-y-auto bg-gray-100 p-4">{children}</main>
     </div>
   );
 }
