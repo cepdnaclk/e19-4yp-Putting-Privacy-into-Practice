6f2b0cdf413e6b5303814b9a816b4ebd133385e0
MohamedFahman <e19105@eng.pdn.ac.lk>
2025-05-31 00:05:16 +0530

    layout

--- code/client/src/components/Layout.jsx
+++ code/client/src/components/Layout.jsx
@@ -4,7 +4,9 @@ export default function Layout({ children }) {
   return (
     <div className="flex h-screen bg-[#f2f2f7] overflow-hidden">
       <Sidebar />
-      <main className="w-full justify-center flex-1 overflow-y-auto bg-gray-100 p-4">{children}</main>
+      <main className="w-full flex justify-center overflow-y-auto bg-gray-100">
+        {children}
+      </main>
     </div>
   );
 }
