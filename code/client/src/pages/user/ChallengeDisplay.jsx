1e6ee9e5dbd5d01996e3ddc81af927990b5f6d6c
MusthakSM <musthakahamed741300@gmail.com>
2025-07-08 05:39:49 +0530

    EssayQuestions challenge has been added

--- code/client/src/pages/user/ChallengeDisplay.jsx
+++ code/client/src/pages/user/ChallengeDisplay.jsx
@@ -1,7 +1,8 @@
 import GameLayout from "../../components/GameLayout";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import McqQuestion from "../../components/McqQuestion";
 import { Clock } from "lucide-react";
+import EssayQuestion from "../../components/EssayQuestion";
 
 const questions = [
   {
@@ -46,21 +47,57 @@ const questions = [
     answer: "B",
     explanation: "Anonymization ensures GDPR compliance.",
   },
+  {
+    id: 3,
+    scenario:
+      "📱 You're building a mobile fitness app that asks users to enter their full name, email, phone number, gender, weight, height, and optionally allow access to location and contacts.",
+    challenge:
+      "As the developer, how would you apply the Data Minimization principle when designing the user registration and data collection process?",
+    type: "essay",
+    answer:
+      "To follow the data minimization principle, I would evaluate which data fields are strictly necessary for the app’s functionality. For registration, name and email might be sufficient. Phone number, gender, and location should be optional unless justified by a feature (e.g., social challenges by location). Access to contacts is not necessary at signup and should be requested only if users opt into social features. Weight and height should be collected only when the user starts using health tracking features, and the user should be informed why this data is needed.",
+    reflection:
+      "This challenge encourages developers to critically analyze each data point they collect, reducing unnecessary processing of personal data. Minimizing data at collection points not only supports GDPR compliance but also builds user trust and reduces potential privacy risks.",
+  },
 ];
 
 export default function QuestionDisplay() {
-  const [currentQuestion, setCurrentQuestion] = useState(questions[0]);
-  const [selectedOption, setSelectedOption] = useState(null);
+  const questionIndex = useRef(0);
+  const [currentQuestion, setCurrentQuestion] = useState(
+    questions[questionIndex.current]
+  );
+  const [userAnswer, setUserAnswer] = useState(null); // user's answer
   const [showExplanation, setShowExplanation] = useState(false);
-  const [timeLeft, setTimeLeft] = useState(30);
+  const [levelEnd, setLevelEnd] = useState(false);
+  const [timeLeft, setTimeLeft] = useState(20);
+  const timerRef = useRef(null);
 
   useEffect(() => {
-    const interval = setInterval(() => {
+    timerRef.current = setInterval(() => {
       setTimeLeft((prev) => (prev > 0 ? prev - 1 : 0));
     }, 1000);
 
-    return () => clearInterval(interval);
-  }, []);
+    return () => clearInterval(timerRef.current);
+  }, [currentQuestion]);
+
+  function handleSubmit() {
+    if (timerRef.current) {
+      clearInterval(timerRef.current);
+    }
+    setShowExplanation(true);
+    if (questionIndex.current + 1 === questions.length) setLevelEnd(true);
+  }
+
+  function showNextQuestion() {
+    setUserAnswer(null);
+    setShowExplanation(false);
+    setCurrentQuestion(questions[++questionIndex.current]);
+    setTimeLeft((prev) =>
+      questions[questionIndex.current].type === "essay" ? prev + 30 : prev + 20
+    );
+  }
+
+  function handleLevelCompletion() {}
 
   return (
     <GameLayout>
@@ -80,23 +117,58 @@ export default function QuestionDisplay() {
           {currentQuestion.type === "mcq" && (
             <McqQuestion
               question={currentQuestion}
-              onSelectOption={setSelectedOption}
-              currentOption={selectedOption}
+              onSelectOption={setUserAnswer}
+              currentOption={userAnswer}
             />
           )}
+
+          {/* essay type questions */}
+          {currentQuestion.type === "essay" && (
+            <EssayQuestion
+              question={currentQuestion}
+              answer={userAnswer}
+              onChange={setUserAnswer}
+            />
+          )}
+
           {/* submit button */}
-          <button
-            className={`py-1 px-3 rounded-md bg-blue-600 ${
-              !selectedOption
-                ? "bg-opacity-50"
-                : "hover:bg-blue-700 cursor-pointer"
-            }`}
-            disabled={!selectedOption}
-          >
-            <span className="text-xs text-white font-semibold">
-              Submit Answer
-            </span>
-          </button>
+          {!showExplanation && (
+            <button
+              className={`py-1 px-3 rounded-md bg-blue-600 ${
+                !userAnswer
+                  ? "bg-opacity-50"
+                  : "hover:bg-blue-700 cursor-pointer"
+              }`}
+              disabled={!userAnswer}
+              onClick={handleSubmit}
+            >
+              <span className="text-xs text-white font-semibold">
+                Submit Answer
+              </span>
+            </button>
+          )}
+          {/* next question button */}
+          {showExplanation && !levelEnd && (
+            <button
+              className="py-1 px-3 rounded-md bg-green-600 hover:bg-green-800 cursor-pointer"
+              onClick={showNextQuestion}
+            >
+              <span className="text-xs text-white font-semibold">
+                Next Challenge
+              </span>
+            </button>
+          )}
+          {/* complete level button */}
+          {levelEnd && (
+            <button
+              className="py-1 px-3 rounded-md bg-green-600 hover:bg-green-800 cursor-pointer"
+              onClick={handleLevelCompletion}
+            >
+              <span className="text-xs text-white font-semibold">
+                Complete Level
+              </span>
+            </button>
+          )}
         </div>
       </div>
     </GameLayout>
